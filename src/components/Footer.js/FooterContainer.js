ef7dd7cf45acf9afb22083e0c07e0cd874f0075b
annaxgrace <anna.grace.conover@gmail.com>
2023-03-07 16:19:08 +0000

    cleaned code and removed warnings

--- src/components/Footer.js/FooterContainer.js
+++ src/components/Footer.js/FooterContainer.js
@@ -1,17 +1,18 @@
 import "./footer.css"
+
 function FooterContainer() {
 
     return (
         <footer className="row justify-content-center">
             <div className="col">
                 <div className="row justify-content-center">
-                    <p class="top-name justify-content-center">ANNA GRACE PENNINGTON</p>
+                    <p className="top-name justify-content-center">ANNA GRACE PENNINGTON</p>
                 </div>
                 <div className="row justify-content-center">
-                    <a href="https://github.com/AnnaxGrace" target="_blank" className="fab fa-github icon fa-3x d-flex " rel="noopener noreferrer"></a>
-                    <a href="https://linkedin.com/in/anna-grace-conover" target="_blank" className="fab fa-linkedin icon fa-3x d-flex" rel="noopener noreferrer"></a>
-                    <div class="git-link">
-                        <a href="mailto:annaxgrace2022@gmail.com" className="fas fa-envelope icon fa-3x d-flex" rel="noopener noreferrer"></a>
+                    <a href="https://github.com/AnnaxGrace" target="_blank" className="fab fa-github icon fa-3x d-flex " rel="noopener noreferrer"> </a>
+                    <a href="https://linkedin.com/in/anna-grace-conover" target="_blank" className="fab fa-linkedin icon fa-3x d-flex" rel="noopener noreferrer"> </a>
+                    <div className="git-link">
+                        <a href="mailto:annaxgrace2022@gmail.com" className="fas fa-envelope icon fa-3x d-flex" rel="noopener noreferrer"> </a>
                     </div>
                 </div>
                 <div className="row justify-content-center">
